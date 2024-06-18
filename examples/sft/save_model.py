4eb769b93bcf40329035caa9abf6a4956a764906
Masahiro Tanaka <mtanaka@microsoft.com>
2024-06-18 17:52:06 +0000

    Updated to script to save hf models

--- examples/sft/save_model.py
+++ examples/sft/save_model.py
@@ -1,11 +1,18 @@
 from transformers import AutoModel, AutoTokenizer
 
-model_name = "meta-llama/Meta-Llama-3-70B"  # 例としてBERTのモデルを使用
-model = AutoModel.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+targets = {
+        "Meta-Llama-3-8B": "meta-llama/Meta-Llama-3-8B",
+        "Llama-2-7b-hf": "meta-llama/Llama-2-7b-hf",
+        "Llama-2-13b-hf": "meta-llama/Llama-2-13b-hf",
+        "Llama-2-70b-hf": "meta-llama/Llama-2-70b-hf",
+}
 
-save_directory = "/blob/projects/ds_rlhf/models/Meta-Llama-3-70B"
+for model_name, model_path in targets.items():
+    model = AutoModel.from_pretrained(model_path)
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
 
-model.save_pretrained(save_directory)
-tokenizer.save_pretrained(save_directory)
+    save_directory = f"/blob/projects/ds_rlhf/models/{model_name}"
+
+    model.save_pretrained(save_directory)
+    tokenizer.save_pretrained(save_directory)
 
