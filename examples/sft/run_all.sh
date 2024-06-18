4b4d5233b28f2fdf9171e1c94e23a8e4a0e0cb61
Masahiro Tanaka <mtanaka@microsoft.com>
2024-06-18 05:23:25 +0000

    add files for benchmarking

---
+++ examples/sft/run_all.sh
@@ -0,0 +1,13 @@
+NUM_NODES=$(wc -l < /job/hostfile)
+LOCKFILE=/tmp/lockfile
+DS_DIR=/scratch/amlt_code/DeepSpeed
+CODE_DIR=/scratch/amlt_code/peft/examples/sft
+
+echo "NUM_NODES: ${NUM_NODES}"
+
+for i in $(seq 1 $((${NUM_NODES} - 1))); do
+    rsync -av --exclude='.git' --exclude='__pycache__' -e ssh ${CODE_DIR}/ node-${i}:${CODE_DIR}/
+    rsync -av --exclude='.git' --exclude='__pycache__' -e ssh ${DS_DIR}/ node-${i}:${DS_DIR}/
+done
+
+ds_ssh "cd /scratch/amlt_code/peft/examples/sft; bash ./run_deepspeed_z3.sh 2>&1 | tee debug.log"
---
+++ examples/sft/run_deepspeed_z3.sh
@@ -0,0 +1,36 @@
+/home/aiscuser/.local/bin/accelerate launch --main_process_ip 10.8.35.136 --main_process_port 12345 \
+--num_machines 1 --num_processes 8 --config_file "configs/deepspeed_config_z3_qlora.yaml"  train.py \
+--seed 100 \
+--model_name_or_path "meta-llama/Meta-Llama-3-70B" \
+--dataset_name "smangrul/ultrachat-10k-chatml" \
+--chat_template_format "chatml" \
+--add_special_tokens False \
+--append_concat_token False \
+--splits "train,test" \
+--max_seq_len 2048 \
+--num_train_epochs 1 \
+--logging_steps 5 \
+--log_level "info" \
+--logging_strategy "steps" \
+--evaluation_strategy "epoch" \
+--save_strategy "epoch" \
+--hub_private_repo True \
+--hub_strategy "every_save" \
+--bf16 True \
+--packing True \
+--learning_rate 1e-4 \
+--lr_scheduler_type "cosine" \
+--weight_decay 1e-4 \
+--warmup_ratio 0.0 \
+--max_grad_norm 1.0 \
+--output_dir "llama-sft-qlora-dsz3" \
+--per_device_train_batch_size 2 \
+--per_device_eval_batch_size 2 \
+--gradient_accumulation_steps 2 \
+--gradient_checkpointing True \
+--use_reentrant True \
+--dataset_text_field "content" \
+--use_flash_attn True \
+--use_peft_lora False \
+--use_4bit_quantization False \
+--use_nested_quant False \
